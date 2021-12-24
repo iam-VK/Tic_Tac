788a13c0f227a081c65d568f2f2fae71666cb41f
iam-VK <vishwakumar.petit@gmail.com>
2021-12-24 21:52:19 +0530

    upgraded UI v1.1.3

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-# x 11 31 22 33
-# o 21 32 23
-# Traceback (most recent call last):
-#   File "D:\Hard Drive\Code Library\python\Tic Tac\main.py", line 70, in <module>
-#     move = random.choice(tuple(chances))
-#   File "D:\Hard Drive\Program Files (Hard Drive)\Python 3.9.6\lib\random.py", line 346, in choice
-#     return seq[self._randbelow(len(seq))]
-# IndexError: tuple index out of range
 import random
+import os
 
 AllElements = {11, 12, 13, 21, 22, 23, 31, 32, 33}
 T = [[11, 12, 13], [21, 22, 23], [31, 32, 33]]
@@ -32,21 +25,94 @@ def searchforindex(matrix, number):
 
 
 def printer():
-    # print("11   12  13")
-    # print("21   22  23")
-    # print("31   32  33")
-
-    print("\n")
-    print("Out :\n")
+    print("o moves to",move,":\n")
     print(" ", T[0][0], " | ", T[0][1], " | ", T[0][2])
     print("--------------------")
     print(" ", T[1][0], " | ", T[1][1], " | ", T[1][2])
     print("--------------------")
     print(" ", T[2][0], " | ", T[2][1], " | ", T[2][2])
-    print("Chances", chances)
-    print("mtE", mtE)
-    print("random", move)
+    print("\n______________________________________________________________")
+    # print("Chances", chances)
+    # print("mtE", mtE)
+    # print("random", move)
     print("\n")
+
+
+def win():
+    print("WINNER IS x\n")
+    os.system("xcowsay -t5 Namba jeychitom Mara & cowsay -f dragon Namba jeychitom Mara | lolcat")
+    os.system("toilet We Won | lolcat")
+
+
+def lost():
+    print("WINNER IS o\n")
+    os.system("xcowthink -t5 Ungala ennamo nu nenaichenA vro, ipadi paniteengaley & cowsay *You: am I a joke to you | lolcat")
+    print("\033[6;32m","**********You LOST**********","\033[0m")
+    os.system("cowsay -f elephant *Computer: hehehe....          Challenge panariya man nee? Ungaluku lam teaching seripattu varadhu coaching dhan| lolcat")
+
+
+def win_chk():
+    global winnerstate
+    # winner choose algorithm
+ # horizontal
+
+    for i in range(0, 3):
+        if T[i] == ['x ', 'x ', 'x ']:
+            win()
+            winnerstate = False
+    for i in range(0, 3):
+        if T[i] == ['o ', 'o ', 'o ']:
+            lost()
+            winnerstate = False
+    
+
+ # vertical
+    if T[0][0] == T[1][0] == T[2][0]:
+        if T[0][0] == 'x ':
+            win()
+        else:
+            lost()
+        winnerstate = False
+    if T[0][1] == T[1][1] == T[2][1]:
+        if T[0][1] == 'x ':
+            win()
+        else:
+            lost()
+        winnerstate = False
+    if T[0][2] == T[1][2] == T[2][2]:
+        if T[0][2] == 'x ':
+            win()
+        else:
+            lost()
+        winnerstate = False
+ 
+
+ # cross
+    if T[0][2] == T[1][1] == T[2][0]:
+        if T[0][2] == 'x ':
+            win()
+        else:
+            lost()
+        winnerstate = False
+    elif T[0][0] == T[1][1] == T[2][2]:
+        if T[0][0] == 'x ':
+            win()
+        else:
+            lost()
+        winnerstate = False
+
+
+# MAIN 
+os.system("figlet Welcome to | lolcat")
+os.system("figlet -w150 The Great Kirikalan | lolcat -p 10000000")
+os.system("figlet Magic show | lolcat")
+print("\nPlot :\n")
+print(" 11 | 12 | 13 ")
+print("---------------")
+print(" 21 | 22 | 23 ")
+print("---------------")
+print(" 31 | 32 | 33 ")
+print("______________________________________________________________\n")
 
 
 while(counter < 4):
@@ -56,17 +122,30 @@ while(counter < 4):
     kk: int = 0
 
     # input from player x
-    ce = int(input("x="))
+    while(True):
+        try:
+            ce = int(input("Your turn x: "))
+        except ValueError:
+            print("\033[6;31m","Error: x supports only integer values within the scope of the table","\033[0m")
+            continue
+        if ce not in mtE:
+            print("\033[6;31m","ERROR: Place occupied, retry another place.","\033[0m")
+        else:
+            break
 
     if 22 not in mtE or ce == 22:
         chances.remove(22)
 
 # marks position for player x
     searchforindex(T, ce)
-    # print(k,kk)
     if T[k][kk] in mtE:
         mtE.remove(T[k][kk])
     T[k][kk] = "x "
+
+# game end algorithm
+    win_chk()
+    if winnerstate == False:
+        break
 
 # main algorithm
     for i in {1, -1, 10, -10}:
@@ -75,7 +154,11 @@ while(counter < 4):
             chances.add(q)
 
 # computer generates random positon for player o NOTE: random.choice does not support set so convert into tuple to use
-    move = random.choice(tuple(chances))
+    try:
+        move = random.choice(list(chances))
+    except IndexError:
+        #print("\nchances:",chances,"\nmove:",move,"\nmtE:",mtE)
+        move = random.choice(list(mtE))
 
 # marks position for player o
     searchforindex(T, move)
@@ -85,38 +168,14 @@ while(counter < 4):
 
     printer()
 
-    # winner choose algorithm
-    # horizontal
-    for i in range(0, 3):
-        if ['x ', 'x ', 'x '] == T[i]:
-            print("Winner is x")
-            winnerstate = False
-
-        elif ['o ' * 3] == T[i]:
-            print("Winner is o")
-            winnerstate = False
-
-    # vertical
-    if T[0][1] == T[1][1] == T[2][1]:
-        print("Winner is", T[0][1])
-        winnerstate = False
-    elif T[0][2] == T[1][2] == T[2][2]:
-        print("Winner is", T[0][2])
-        winnerstate = False
-
-    # cross
-    elif T[0][2] == T[1][1] == T[2][0]:
-        print("Winner is", T[0][2])
-        winnerstate = False
-    elif T[0][0] == T[1][1] == T[2][2]:
-        print("Winner is", T[1][1])
-        winnerstate = False
-
+# game end algorithm
+    win_chk()
     if winnerstate == False:
         break
 
-# game end algorithm
     counter += 1
 
 if winnerstate:
     print("Match DRAW")
+    os.system("xcowsay -t5 You can Do it BUGGAA")
+print("\n______________________________________________________________\n")
